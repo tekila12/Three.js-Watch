549730268615203bb2bb2cfc6240a60249478e76
Ajnur <radovic.ajnur96@gmail.com>
2022-11-30 04:16:19 +0100

    finished version

--- src/App.js
+++ src/App.js
@@ -2,20 +2,21 @@ import './App.css';
 import {Canvas} from '@react-three/fiber'
 import { Watch } from './Watch';
 import { ContactShadows, PresentationControls } from '@react-three/drei';
+import { Suspense } from 'react';
 
 function App() {
   return (
     <div className="App">
-     <nav class="navbar navbar-expand-lg bg-light">
-  <div class="container-fluid">
+     <nav className="navbar navbar-expand-lg bg-light">
+  <div className="container-fluid">
     
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">MONACO</a>
+    <div className="collapse navbar-collapse" id="navbarNav">
+      <ul className="navbar-nav">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#">MONACO</a>
         </li>
   
       </ul>
@@ -27,30 +28,38 @@ function App() {
   <div className='col-lg-5 col-md-5 col-sm-12 d-flex align-items-center justify-content-center text-center'>
 <h3 className='display-4 text-center '>Check out our best MONACO watch</h3>
 </div>
-<div class='col-lg-6 col-md-5 col-sm-12'>
-<Canvas shadows dpr={[1,2]} camera={{position:[0,0,4], fov:50}}
+<div className='col-lg-6 col-md-5 col-sm-12'>
+ 
 
-style={{height:"500px"}}>
-<ambientLight intensity={0.2} />
-<spotLight position={[10,10,10]}
- angle={0.15}
-  penumbra={1}
-   shadow-mapSize={[512,512]}
-   castShadow/>
-<PresentationControls global snap={{mass:3, tension:1500}}
-config={{mass:2, tension:500}}
-polar={[-Math.PI /3, Math.PI /2]}
-azimuth={[-Math.PI /1,4, Math.PI /2]}>
-<Watch rotation={[Math.PI /2, 9.46, 3.1]} position={[0, 0.25,1]} scale={0.0022}/>
-</PresentationControls>
-<ContactShadows  rotateX={-Math.PI /2} position={[0, -1.4, 0]} opacity={0.75}
-width={10}
-height={10}
-blur={2.3}
-far={2}/>
-</Canvas>
-</div>
-</div>
+
+            <Canvas shadows dpr={[1, 2]}
+             camera={{ position: [0, 0, 4],
+               fov: 50 }}
+              style={{ height: "500px" }}>
+              <ambientLight intensity={0.2} />
+              <spotLight position={[10, 10, 10]}
+                angle={0.15}
+                penumbra={1}
+                shadow-mapSize={[512, 512]}
+                castShadow />
+              <PresentationControls global snap={{ mass: 3, tension: 1500 }}
+                config={{ mass: 2, tension: 500 }}
+                polar={[-Math.PI / 3, Math.PI / 2]}
+                azimuth={[-Math.PI / 1, 4, Math.PI / 2]}>
+                  
+                <Suspense fallback={null}>
+                  <Watch rotation={[Math.PI / 2, 1.5, 1]} position={[-0.5, 0.55, 1]} scale={0.0024} />
+                </Suspense>
+
+              </PresentationControls>
+              <ContactShadows rotation-X={Math.PI / 2} position={[1, -2.4, 0]} opacity={0.75}
+                width={10}
+                height={10}
+                blur={2.3}
+                far={1} />
+            </Canvas>
+          </div>
+        </div>
 </div>
 
     </div>
--- src/Watch.js
+++ src/Watch.js
@@ -1,17 +1,30 @@
 import React from 'react'
-import {useGLTF} from '@react-three/drei'
+import {Html, useGLTF} from '@react-three/drei'
+import { useFrame } from '@react-three/fiber'
 export const  Watch = (props) =>  {
     const ref= React.useRef()
 
     const {nodes, materials} =useGLTF('./watch-v1.glb')
+    useFrame((state)=>{
+      const t =  state.clock.getElapsedTime();
+      ref.current.rotation.x = -Math.PI /1.7 - Math.cos(t/4)/8
+      ref.current.rotation.y =  Math.sin(t/1)/8
+      ref.current.rotation.z = -Math.PI /-10 + Math.sin(t/1.5)/20
+      ref.current.position.y =   Math.sin(t/1.5)/10
+    
+
+
+
+    })
   return (
-<>
-          <group ref={ref} shadowMap  {...props} dispose={null}>
 
-              <mesh castShadow receiveShadow geometry={nodes.Object006_watch_0.geometry}
-                  material={materials.watch} />
+    <group ref={ref} shadowMap  {...props} dispose={null}>
+      <Html scale={100} rotation={[Math.PI / 2, 0, 0]} position={[180, -450, -240]} transform >
+        <div className='annotation'>6000$</div>
+      </Html>
+      <mesh castShadow receiveShadow geometry={nodes.Object006_watch_0.geometry}
+        material={materials.watch} />
+    </group>
 
-          </group>
-</>
   )
 }
