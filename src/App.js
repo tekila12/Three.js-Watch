0b07ef3f8adec05e73149e2c082c784a7234da60
Ajnur <radovic.ajnur96@gmail.com>
2022-11-30 04:32:01 +0100

    ...

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
     <div className="collapse navbar-collapse" id="navbarNav">
       <ul className="navbar-nav">
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" >MONACO</a>
+          <a className="nav-link active" aria-current="page" href="Home">MONACO</a>
         </li>
   
       </ul>
